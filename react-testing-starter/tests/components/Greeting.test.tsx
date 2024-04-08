dc33135e8c2fd226409e4f6116c77590aaeb61c3
Adrian John <adrianjohn.developer@gmail.com>
2024-04-08 15:41:15 -0500

    simplify test setup

--- react-testing-starter/tests/components/Greeting.test.tsx
+++ react-testing-starter/tests/components/Greeting.test.tsx
@@ -1,7 +1,6 @@
-import {it, expect, describe } from 'vitest';
 import {render, screen} from '@testing-library/react';
 import Greet from '../../src/components/Greet';
-import '@testing-library/jest-dom/vitest';
+
 
 describe('Greet', () =>{
     it('Render Hello when name is not provided', ()=>{
