732ab08a481552193db0ccccd102c1dd721727a8
Adrian John <adrianjohn.developer@gmail.com>
2024-04-08 15:33:18 -0500

    test greeting page

---
+++ react-testing-starter/tests/components/Greeting.test.tsx
@@ -0,0 +1,23 @@
+import {it, expect, describe } from 'vitest';
+import {render, screen} from '@testing-library/react';
+import Greet from '../../src/components/Greet';
+import '@testing-library/jest-dom/vitest';
+
+describe('Greet', () =>{
+    it('Render Hello when name is not provided', ()=>{
+        render(<Greet name='Adrian'/>);
+
+        const heading = screen.getByRole('heading');
+        expect(heading).toBeInTheDocument();
+        expect(heading).toHaveTextContent(/Adrian/i);
+
+    });
+    it('Render Hello when name is not provided', ()=>{
+        render(<Greet/>);
+
+        const button = screen.getByRole('button');
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveTextContent(/login/i);
+
+    });
+})
